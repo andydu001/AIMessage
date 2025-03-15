a20a8ff9c4ff122f2fe0b27a4ab30e1fda1933b3
Andy Duverneau <77910275+andydu001@users.noreply.github.com>
2025-03-14 23:57:23 -0400

    Delete HAG902/src/main/java/org/example/Main.java

--- HAG902/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-import dev.langchain4j.data.audio.Audio;
-public class Main {
-    public static void main(String[] args) {
-        new OP09();
-        new ChatMA01();
-    }
-}
